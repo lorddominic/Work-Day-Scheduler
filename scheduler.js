d14268c65e2a961b8078c0cdd9cbb105416e46f6
Boyan Xu <developerxu123@gmail.com>
2020-02-13 15:17:04 -0700

    updated

--- scheduler.js
+++ scheduler.js
@@ -24,16 +24,25 @@ $(document).ready(function () {
     function updateBackground() {
 
         //get the time using moment.js
+        // var currentTime = $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
+        var currentHour = moment().hour();
 
-        // compare the current time with the time block time 
-
-
-        //add and remove classes based off of if the current time is before or after the block time
-
-
-
-
+        $(".time-block").each(function () {
+            var hour = parseInt($(this).attr("id").split("-")[1]);
+            if (hour < currentHour) {
+                $(this).addClass("past");
+            }
+            else if (hour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
     }
 
     updateBackground();
@@ -41,13 +50,16 @@ $(document).ready(function () {
 
     //get values from local storage and set the text areas
 
+    // console.log($("#hour-9 .description").siblings());
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-
-
-
-
-
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 });
 
